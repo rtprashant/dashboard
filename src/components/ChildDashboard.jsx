507345dcbb6f289053cbb3586a0440c4bc667e3e
prashant <prashantrajput827327@gamil.com>
2024-08-26 23:10:43 +0530

    second commit

--- src/components/ChildDashboard.jsx
+++ src/components/ChildDashboard.jsx
@@ -1,25 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DummayData from './DummyData.json'
 import { Pie } from 'react-chartjs-2'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from 'chart.js'
+ChartJS.register(ArcElement, Tooltip, Legend, Title)
 
 
-function ChildDashboard({dashboard_name , widget_name}) {
+function ChildDashboard({ dashboard_name, widget_name }) {
+
+
+  const selectedCategory = DummayData.dashboard.categories.find((category) => category.name === dashboard_name)
+  console.log(selectedCategory);
+
+  // const selectedwidget =selectedCategory?.widgets.map(
+  // (widget)=>widget.name)
+  // console.log(selectedwidget);
+
+  const selectedWidget = selectedCategory?.widgets.find(
+    (widget) => widget.name === widget_name
+  );
+  console.log(selectedWidget);
+  const labels = selectedWidget.data.labels;
+  const colors = selectedWidget.data.datasets[0].backgroundColor;
+  if (!selectedWidget || !selectedWidget.data || !selectedWidget.data.labels) {
+    return (
+      <div className='flex flex-col gap'>
+        <div className='font-sans font-bold text-[14px] ml-4'>
+          {dashboard_name}
+        </div>
+        <div className='w-1/3 h-64 rounded-3xl bg-[#F2F0F5] flex items-center justify-center'>
+          <div className='w-[93%] h-[90%] bg-white rounded-xl p-4'>
+            <div className='font-sans font-bold text-[13px]'>
+              No Data Available
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+
+
   return (
     <div>
-        <div className='flex flex-col gap'>
-            <div className='font-sans font-bold text-[14px] ml-4'>{dashboard_name}</div>
-            <div className='w-1/3 h-64 rounded-3xl bg-[#F2F0F5] flex items-center justify-center'>
-            <div className='w-[93%] h-[90%] bg-white rounded-xl p-4'>
-                <div className='font-sans font-bold text-[13px] '>
-                    {widget_name}
-                </div>
+      {(<div className='flex flex-col  '>
 
+        <div className='w-[30vw] h-64 rounded-3xl bg-[#F2F0F5] flex items-center justify-center  '>
+          <div className='w-[93%] h-[90%] bg-white rounded-xl p-5 '>
+            <div className='font-sans font-bold text-[13px]  w-[20vw] h-[13vw] flex '
+            >
+              {widget_name}
+              <div className=' '>
+                <Pie options={selectedWidget.options} data={selectedWidget.data} />
+              </div>
+              <div className='flex flex-col items-start mt-2 ml-10 p-2'>
+                {/* Align labels with corresponding colors */}
+                {labels.map((label, index) => (
+                  <div key={index} className='flex items-center mb-1 gap-3 '>
+                    <div
+                      className='w-2 h-2 rounded-full '
+                      style={{ backgroundColor: colors[index] }}
+                    ></div>
+                    <span className='text-[10px] w-full'>{label}</span>
+                  </div>
+                ))}
+              </div>
             </div>
-            
 
-            </div>
+          </div>
+
 
         </div>
-      
+
+      </div>)}
+
+
     </div>
   )
 }
--- src/components/ParentDashboard.jsx
+++ src/components/ParentDashboard.jsx
@@ -1,63 +1,120 @@
+import { useState } from 'react'
 import React from 'react'
 import ChildDashboard from './ChildDashboard'
 import DummyData from './DummyData.json'
 
 
-function ParentDashboard() {
-   
-    const w_name ='cdcc'
-    const d_names = 'xyz'
-    // const d_names = DummyData?.dashboard.categories?.map((cat) => cat.name) || [];
-    // const firstCategory = DummyData?.dashboard?.categories[0];
-    // const firstWidget = firstCategory?.widgets?.[0];
-    // const graphopt = firstWidget?.options;
-    // const graphdata = firstWidget?.data;
 
-    if (d_names.length === 0) {
-      return <div>No categories available</div>;
+function ParentDashboard() {
+   const [dashboard, setdeshboard] = useState(DummyData.dashboard.categories);
+   const [isOpen , setIsOpen] = useState(false)
+   const [selectedCategory, setSelectedCategory] = useState('');
+    const [newWidgetName, setNewWidgetName] = useState('');
+    const [newWidgetType, setNewWidgetType] = useState('');
+
+    const handleWidgetAdd = ()=>{
+        console.log('clicked');
+        
+        setIsOpen(true)
+        console.log(isOpen)
     }
-  
+    const handleWidgetClose = ()=>{
+        setIsOpen(false)
+    }
+
   return (
     <div>
-        <div className='bg-[#F0F5F9] p-10'>
-           <div className='flex justify-between'>
-           <div className='font-sans font-bold'>
-               CNAPP Dashboard
-            </div>
-            <div className='flex gap-3'>
-                <div className='border text-[12px] p-2 font-semibold text-gray-500 rounded-lg px-3'>Add Widget +</div>
-                <div className='border text-[12px] p-2 font-semibold text-gray-500 rounded-lg px-3'>
-                <i class="fa-solid fa-arrows-rotate"></i>
-                </div>
-                <div className='border text-[12px] p-2 font-semibold text-gray-500 rounded-lg px-3'>
-                <i class="fa-solid fa-ellipsis-vertical"></i>
-                </div>
-                <div className='border border-blue-900  p-2 font-semibold text-blue-900 rounded-lg px-3 flex gap-1'>
-                    <div>
-                    <i class="fa-solid fa-clock"></i>
-                    </div>
-                    <div className='border border-blue-900 '>
+       {!isOpen && (
+         <div className='bg-[#F0F5F9] p-10'>
+         <div className='flex justify-between'>
+         <div className='font-sans font-bold'>
+             CNAPP Dashboard
+          </div>
+          <div className='flex gap-3'>
+              <button className='border text-[12px] p-2 font-semibold text-gray-500 rounded-lg px-30' onClick={handleWidgetAdd}>Add Widget +</button>
+              <div className='border text-[12px] p-2 font-semibold text-gray-500 rounded-lg px-3'>
+              <i class="fa-solid fa-arrows-rotate"></i>
+              </div>
+              <div className='border text-[12px] p-2 font-semibold text-gray-500 rounded-lg px-3'>
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+              </div>
+              <div className='border border-blue-900  p-2 font-semibold text-blue-900 rounded-lg px-3 flex gap-1'>
+                  <div>
+                  <i class="fa-solid fa-clock"></i>
+                  </div>
+                  <div className='border border-blue-900 '>
 
-                    </div>
-                    <div>
-                        <select name="" className='bg-transparent text-[15px]  ring-blue-900 border-none'>
-                        <option value="0">Last 24 Hours</option>
-                            <option value="1">Last 2 Days</option>
-                            <option value="2">Last 3 Days</option>
-                            <option value="3">Last 4 Days</option>
-                            <option value="4">Last 5 Days</option>
-                        </select>
+                  </div>
+                  <div>
+                      <select name="" className='bg-transparent text-[15px]  ring-blue-900 border-none'>
+                      <option value="0">Last 24 Hours</option>
+                          <option value="1">Last 2 Days</option>
+                          <option value="2">Last 3 Days</option>
+                          <option value="3">Last 4 Days</option>
+                          <option value="4">Last 5 Days</option>
+                      </select>
 
-                    </div>
-                </div>
+                  </div>
+              </div>
 
-            </div>
-           </div>
-           <ChildDashboard dashboard_name={d_names} widget_name={w_name}/>
-           
-
-
+          </div>
+         </div>
+         
+         {/* {dashboard.map((dash) => (
+          <div key={dash.id} className=''> 
+          {dash.widgets.map((wid)=>(<ChildDashboard key={wid.id} dashboard_name={dash.name
+          } widget_name={wid.name}/>))}
+              </div> 
+          
+          ))} */}
+          {dashboard.map((category) => (
+      <div key={category.id} className='my-4'>
+        <div className='font-sans font-bold text-[14px] ml-4'>
+          {category.name}
         </div>
+        <div className='flex flex-wrap gap-4 '>
+          {category.widgets.map((widget) => (
+            <ChildDashboard
+              key={widget.id}
+              dashboard_name={category.name}
+              widget_name={widget.name}
+            />
+          ))}
+        </div>
+      </div>
+    ))}
+         
+
+
+      </div>
+       )}
+
+        {isOpen && (<div>
+            <div className='modal absolute z-[50] '>
+                        <div className='modal-content'>
+                            <h3>Add New Widget</h3>
+                            <label>
+                                Select Category:
+                                <select onChange={(e) => setSelectedCategory(e.target.value)} value={selectedCategory}>
+                                    <option value="">Select Category</option>
+                                    {dashboard.map((category) => (
+                                        <option key={category.id} value={category.id}>{category.name}</option>
+                                    ))}
+                                </select>
+                            </label>
+                            <label>
+                                Widget Name:
+                                <input type='text' onChange={(e) => setNewWidgetName(e.target.value)} value={newWidgetName} />
+                            </label>
+                            <label>
+                                Widget Type:
+                                <input type='text' onChange={(e) => setNewWidgetType(e.target.value)} value={newWidgetType} />
+                            </label>
+                            <button onClick={handleWidgetClose}>close</button>
+                            
+                        </div>
+                    </div>
+        </div>)}
        
       
     </div>
